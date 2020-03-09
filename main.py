9736af41a5079ee8d6ae86d4e111643ebf08807e
David Domínguez <dominguezbarradavid33@gmail.com>
2020-03-09 10:28:45 +0100

    Add weights save and load

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from random import random, choice
-import tqdm
+import os
+import datetime
 from collections import deque
+import statistics as stt
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Model
@@ -68,13 +70,23 @@ def my_dqn_agent_exploring(observation, configuration):
     return action
 
 def play(agent_1, agent_2, render = True):
-    data = env.run([agent_1, agent_2])
-    # env.render()
-    result = {1.:'WIN',.5:'DRAW',0.:'LOSE'}
+    if agent_2 == 'random':
+        data = [env.run([agent_1, agent_2]) for i in range(10)]
+        wins = sum(d[-1][0]['reward']==1. for d in data)
+        draws = sum(d[-1][0]['reward']==0.5 for d in data)
+        loses = sum(d[-1][0]['reward']==0. for d in data)
+        result = {0:'WIN',1:'DRAW',2:'LOSE'}[([wins,draws,loses].index(max([wins,draws,loses])))]
+        result = "{}({}/{}/{})".format(result,wins,draws,loses)
+        moves = stt.mean([len(d) for d in data])
+    else:
+        data = env.run([agent_1, agent_2])
+        result_num = data[-1][0]['reward']
+        result = {1.:'WIN ',.5:'DRAW',0.:'LOSE'}[result_num]
+        moves = len(data)
     if id(agent_1) == id(agent_2):
         agent_2 = 'yourself'
-    print(f"YOU {result[data[-1][0]['reward']]} against {agent_2}")
-    print(f"Moves: {len(data)}")
+    print(f"YOU {result} against {agent_2}")
+    print(f"Moves: {moves}")
     if render:
         print("final state:")
         env.render()
@@ -95,33 +107,42 @@ class MyDQN(Model):
     def __init__(self, conf):
         super().__init__()
         self.no = conf.columns
-        self.conv1 = Conv2D(64, 4, padding='same', activation='linear')
-        self.maxp1 = tf.keras.layers.MaxPool2D()
-        self.relu1 = tf.keras.layers.ReLU()
-        self.conv2 = Conv2D(32, 3, padding='same', activation='relu')
+        self.conv1 = Conv2D(32, 5, padding='same', activation='relu')
+        self.conv2 = Conv2D(64, 4, padding='same', activation='relu')
+        self.conv3 = Conv2D(128, 3, padding='same', activation='relu')
+        self.conv4 = Conv2D(256, 2, padding='same', activation='relu')
         self.flatten = Flatten()
-        self.d1 = Dense(128, activation='relu')
-        self.d2 = Dense(64, activation='relu')
-        self.d3 = Dense(32, activation='relu')
-        self.d4 = Dense(self.no, activation=None)
+        self.d1 = Dense(256, activation='relu')
+        self.d2 = Dense(128, activation='relu')
+        self.d3 = Dense(64, activation='relu')
+        self.d4 = Dense(32, activation='relu')
+        self.d5 = Dense(self.no, activation=None)
         self.act = lambda x: np.argmax(x, axis=-1)
         self.sel = lambda a, x: tf.reduce_sum(x*tf.one_hot(a, model.no), axis=-1)
         self.max = lambda x: tf.reduce_max(x, axis=-1)
 
     def call(self, x):
         x = self.conv1(x)
-        x = self.maxp1(x)
-        x = self.relu1(x)
         x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
         x = self.flatten(x)
         x = self.d1(x)
         x = self.d2(x)
         x = self.d3(x)
         x = self.d4(x)
+        x = self.d5(x)
         return x
 
 alpha = 0.1
 gamma = 1.
+model = MyDQN(env.configuration)
+loss_object = tf.keras.losses.MeanSquaredError()
+optimizer = tf.keras.optimizers.Nadam()
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.Mean(name='train_accuracy')
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.Mean(name='test_accuracy')
 
 @tf.function
 def train_step(states, actions, rewards, next_states):
@@ -141,39 +162,48 @@ def train_step(states, actions, rewards, next_states):
     train_loss(loss)
     # train_accuracy(new_expected_act_rewards, expected_act_rewards)
 
-model = MyDQN(env.configuration)
-loss_object = tf.keras.losses.MeanSquaredError()
-optimizer = tf.keras.optimizers.Adam()
-train_loss = tf.keras.metrics.Mean(name='train_loss')
-train_accuracy = tf.keras.metrics.Mean(name='train_accuracy')
-test_loss = tf.keras.metrics.Mean(name='test_loss')
-test_accuracy = tf.keras.metrics.Mean(name='test_accuracy')
-
-
 # Play as first position against random agent.
-def load_memory(batch = 20, memory_max_size = 1000):
-    memory = Memory(max_size = memory_max_size)
-    trainer = env.train([None, "random"])
+def load_memory(env, conf, batch = 20, max_size = 1000):
+    memory = Memory(max_size = max_size)
     done = True
     for _ in range(batch):
         if done:
-            observation = trainer.reset()
-        action = my_agent(observation, env.configuration)
-        next_observation, reward, done, info = trainer.step(my_agent(observation, env.configuration))
+            observation = env.reset()
+        action = my_agent(observation, conf)
+        next_observation, reward, done, info = env.step(my_agent(observation, conf))
         experience = observation.board, action, reward, next_observation.board
         memory.add(experience)
         observation = next_observation
     return memory
 
+def load_model(model):
+    """Load the weights of the model (mutate it) and return the recomended path to save it"""
+    weights_path = "res/weights"
+    _path = []
+    for d in weights_path.split('/'):
+        _path.append(d)
+        try:
+            os.mkdir('/'.join(_path))
+        except FileExistsError:
+            pass # It makes no sense to create it if already exists
+    weights_name_load_files = sorted(os.listdir(weights_path), reverse=True)
+    if weights_name_load_files:
+        model.load_weights(os.path.join(weights_path, weights_name_load_files[0]))
+    weights_name_save = 'weights'+datetime.datetime.now().isoformat(timespec='seconds')+'.h5'
+    return os.path.join(weights_path, weights_name_save)
+
+
+EPOCHS = 100
 batch = 20
-memory = load_memory(batch, 1000000)
+memory_trainer = env.train([None, "random"])
+memory = load_memory(memory_trainer, env.configuration, batch=batch, max_size=1000000)
 trainer1p = env.train([None, my_dqn_agent])
 trainer2p = env.train([my_dqn_agent, None])
 lets_explore = 0.2
 total_epochs = 0
-while True:
-    EPOCHS = 10
-    template = "Epoch {}/{}, Loss: {}, Moves: {}"
+weights_file_save = load_model(model)
+while False:
+    template = "Epoch {:02}/{}({}), Loss: {}, Moves: {}"
     # for epoch in tqdm.tqdm(range(EPOCHS)):
     for epoch in range(EPOCHS):
         total_epochs += 1
@@ -193,19 +223,20 @@ while True:
             experiences_tensor = raw2tensor(experiences)
             train_step(*experiences_tensor)
             observation = next_observation
-        print(template.format(epoch+1, total_epochs, train_loss.result(), moves))
+        print(template.format(epoch+1, EPOCHS, total_epochs, train_loss.result(), moves))
     
         train_loss.reset_states()
         # env.render(mode="human", width=100, height=90, header=False, controls=False)
-    
+    model.save_weights(weights_file_save)
     
     # Playing versus random
     print("LETS PLAY\n")
     play(my_dqn_agent, 'random', render = False)
     play(my_dqn_agent, 'negamax', render = False)
-    play(my_dqn_agent, my_dqn_agent, render = False)
-    # print("YOU PLAY NOW! YOU CAN WIN!!!!")
-    # try:
-    #     play(my_me, my_dqn_agent)
-    # except KeyError:
-    #     pass
+    # play(my_dqn_agent, my_dqn_agent, render = False)
+print("YOU PLAY NOW! YOU CAN WIN!!!!")
+try:
+    play(my_me, my_dqn_agent)
+    # play(my_me, 'negamax')
+except KeyError:
+    pass
